6915204b7a85bdaa097a3eeba27d38decfe1666c
entest <hai@entest.io>
2022-07-30 06:55:02 +0000

    update reade

--- bin/eventbridge-demo.ts
+++ bin/eventbridge-demo.ts
@@ -3,6 +3,8 @@ import * as cdk from "aws-cdk-lib";
 import { EventbridgeDemoStack } from "../lib/eventbridge-demo-stack";
 
 const app = new cdk.App();
+
+// topic arn is optional - when want lambda send sns
 new EventbridgeDemoStack(app, "EventbridgeDemoStack", {
   topicArn: `arn:aws:sns:${process.env.CDK_DEFAULT_REGION}:${process.env.CDK_DEFAULT_ACCOUNT}:MonitorEc2`,
 });
